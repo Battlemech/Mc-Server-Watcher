b7529b144c820eec7084b909dad1b1d8d9120e71
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2024-06-09 16:46:07 +0200

    add hyperparams

--- src/interface/mc_server_interface.py
+++ src/interface/mc_server_interface.py
@@ -5,12 +5,12 @@ from src.interface.server_tracker import IsServerAbandonded
 from src.interface.server_commands import send_command, setup_command_sender
 from src.threading_util import ensure_thread_start
 
-def run_server():
+def run_server(min_gb: int = 1, max_gb: int = 7, server_folder: str = '../IER Serverpack 4.3.1/'):
     try:
         # Start a subprocess with the command 'java -Xmx7G -Xms6G -jar forge-1.12.2-14.23.5.2860.jar nogui'
         # The subprocess is executed in the '/path/to/server/folder' directory
         # The stdout and stderr of the subprocess are redirected to pipes
-        server_process = subprocess.Popen(['java', '-Xmx7G', '-Xms6G', '-jar', 'forge-1.12.2-14.23.5.2860.jar', 'nogui'], cwd='/path/to/server/folder', stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        server_process = subprocess.Popen(['java', f'-Xmx{max_gb}G', f'-Xms6{min_gb}', '-jar', 'forge-1.12.2-14.23.5.2860.jar', 'nogui'], cwd=server_folder, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 
         # allows sending commands to the server
         command_thread = setup_command_sender(server_process)
