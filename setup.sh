b61fe96a5faf0fc6bebe49324343cba049bd9a5f
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2024-06-12 20:57:58 +0200

    start prototype

--- setup.sh
+++ setup.sh
@@ -18,8 +18,29 @@ sudo yum install python3-pip -y
 pip3 install --upgrade pip
 
 # create virtual python environment
-python3 -m venv server
-source server/bin/activate
+python3 -m venv venvServer
+source venvServer/bin/activate
 
 # install mcstatus
 pip install mcstatus
+
+# deactivate virtual python environment
+deactivate
+
+# ensure "start.sh" is executable
+chmod +x start.sh
+
+# install screen
+sudo yum install screen -y
+
+# create service file, using template at ./McServer.service
+sudo cp McServer.service /etc/systemd/system/McServer.service
+
+# enable service
+sudo systemctl enable McServer
+
+# start service
+sudo systemctl start McServer
+
+# check status
+sudo systemctl status McServer
---
+++ start.sh
@@ -0,0 +1,8 @@
+# activate virtual python environment "venvServer"
+source venvServer/bin/activate
+
+# start server by running main.py
+python main.py
+
+# deactivate virtual python environment "venvServer"
+deactivate
