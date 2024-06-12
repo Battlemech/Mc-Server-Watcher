7230bb4e009edc04faa936ffcd50e26233328275
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2024-06-12 19:33:08 +0200

    remove zip

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ sudo yum install wget -y
 
 # download server pack, unzip and remove zip
 wget https://mediafilez.forgecdn.net/files/5128/918/IER%20Serverpack%204.3.1.zip
+unzip "IER\ Serverpack\ 4.3.1.zip"
+rm "IER\ Serverpack\ 4.3.1.zip"
