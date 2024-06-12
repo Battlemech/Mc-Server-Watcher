2b05214c10a55f2f2b6ded080a6bc19e73cba3aa
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2024-06-12 20:04:14 +0200

    install java

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
-# install java
-sudo yum install java-11-amazon-corretto-headless -y
+# uninstall java 11
+sudo yum remove java-11-amazon-corretto-headless -y
+
+# install java 8
+sudo yum install java-8-amazon-corretto-headless -y
 
 # install wget
 sudo yum install wget -y
@@ -8,3 +11,15 @@ sudo yum install wget -y
 wget https://mediafilez.forgecdn.net/files/5128/918/IER%20Serverpack%204.3.1.zip
 unzip "IER Serverpack 4.3.1.zip"
 rm "IER Serverpack 4.3.1.zip"
+
+# install pip
+sudo yum install python3-pip -y
+# update pip
+pip3 install --upgrade pip
+
+# create virtual python environment
+python3 -m venv server
+source server/bin/activate
+
+# install mcstatus
+pip install mcstatus
