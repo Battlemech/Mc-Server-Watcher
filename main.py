f5d4c73f35f16798607203012021e3a183b2bf8e
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2024-06-12 21:02:59 +0200

    output start time

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from src.mc_server_interface import MCServerInterface
 import os
+from datetime import datetime
 
 def main():
+    # create a simple log file, printing current time
+    with open("log.txt", "w") as log:
+        log.write(f"Server started at {datetime.now()}!\n")
+
     # run the server until no players are online
     server = MCServerInterface()
     server.waitForExit()
