4381414a6eec9f095c5467bc78c0cbfc0e77e123
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2024-06-12 19:22:54 +0200

    add setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# install git
+sudo yam install git -y
+
+# install java
+sudo yum install java-1.8.0-openjdk -y
+
+# install wget
+sudo yum install wget -y
+
+# download server pack, adding it to the server directory
+wget https://www.curseforge.com/api/v1/mods/339496/files/5128918/download
